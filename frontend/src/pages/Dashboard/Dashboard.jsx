89c125d9d2c6f674dd0bf700ddb5178e87639944
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2026-04-06 18:12:46 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ import { useState, useEffect, useRef } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://discussai-27pf.onrender.com/');
 
 const Dashboard = () => {
   const { logout, user, aiPreferences, updateAiPreferences } = useAuth();
